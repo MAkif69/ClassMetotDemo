b9c76cac12f2d18956436fd9f753bdef7f7f817f
M.Akif <M.Akif@DESKTOP-HHK6G12>
2021-01-19 21:27:13 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri) 
+        {
+            Console.WriteLine("Müşteri eklendi:"+ "\n" + "Adı:"+" " + musteri.Ad+ " " + " Sehri:"+ " " 
+                +musteri.Sehir+ " "+"Yaşı:"+ " " +musteri.Yas);
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi:" + "\n" + "Adı:" + " " + musteri.Ad + " " + " Sehri:" + " "
+                + musteri.Sehir + " " + "Yaşı:" + " " + musteri.Yas);
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Listelendi:" + "\n" + "Adı:" + " " + musteri.Ad + " " + " Sehri:" + " "
+                + musteri.Sehir + " " + "Yaşı:" + " " + musteri.Yas);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "M.Akif";
+            musteri1.Sehir = "Bayburt";
+            musteri1.Yas = 25;
+
+            MusteriManager musteriManager = new MusteriManager();
+            Console.WriteLine("Ekle metodu çalışır \n");
+            musteriManager.Ekle(musteri1);
+
+            Console.WriteLine("Sil metodu çalışır \n");
+            musteriManager.Sil(musteri1);
+
+            Console.WriteLine("Listele metodu çalışır \n");
+            musteriManager.Listele(musteri1);
+
+            
+        }
+    }
+}
